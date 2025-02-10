c7fad873eeaab56e4c0fc54d4669181bb19f61e4
evespimrose <evespimrose@users.noreply.github.com>
2025-02-10 17:47:49 +0900

    [Feat] 스킬 메커니즘 정리 및 스킬 추가
    [Body]
    - UniTask 추가
    - 장풍 에셋 추가
    - 가시 에셋 추가
    - 스킬 디스펜서에 등록 시 자동으로 유니태스크 돌려서 투사체 발사하도록
    
    [TODO]
    - 디스펜서에 가시 등록시켜서 "실제로" 로직이 진행되는지 확인
    - 장풍 프리팹 제작해야됨
    - 몬스터 충돌 로직 ITakeDamage 활용해 적용해야됨
    - 플레이어 위치 받아와야됨. 스킬 디스펜서 transform으로 해결 가능할듯 함.

--- Assets/_Scripts/Manager/ProjectileManager.cs
+++ Assets/_Scripts/Manager/ProjectileManager.cs
@@ -10,24 +10,20 @@ public class ProjectileManager : Singleton<ProjectileManager>
 {
     public List<Projectile> activeProjectiles = new List<Projectile>();
 
-    public void SpawnProjectile(SkillName skillName)
+    public void SpawnProjectile(Enums.SkillName skillName)
     {
         // TODO : create correct Projectile by SkillName with switch
-        
+
         Vector3 startPosition = Vector3.zero; // TODO : player pos
         Vector3 direction = Random.insideUnitCircle.normalized; // TODO : closest monster
         float speed = 1f;
-        activeProjectiles.Add(new Projectile(startPosition, direction, speed));
+
+
+        //activeProjectiles.Add(new Projectile(startPosition, direction, speed));
     }
 
     private void Update()
     {
-        if (activeProjectiles.Count > 0)
-        {
-            for (int i = 0; i < activeProjectiles.Count; i++)
-            {
-                activeProjectiles[i].playerPosition += activeProjectiles[i].direction * activeProjectiles[i].speed * Time.deltaTime;
-            }
-        }
+
     }
 }
---
+++ Assets/_Scripts/Player/Skill/Skill.cs
@@ -0,0 +1,51 @@
+using UnityEngine;
+using Cysharp.Threading.Tasks;
+
+public class Skill
+{
+    public Enums.SkillName skillName;
+    public float cooldown;
+    private float defaultCooldown;
+
+    private bool isSkillActive = false;
+
+    private async void Start()
+    {
+
+        await StartSkill();
+    }
+
+    private async UniTask StartSkill()
+    {
+        isSkillActive = true;
+
+        while (isSkillActive)
+        {
+            if (!GameManager.Instance.isPaused)
+            {
+                Debug.Log($"Fire! : {skillName}");
+                Fire();
+                await DelayFloat(defaultCooldown);
+            }
+            else
+            {
+                await UniTask.Yield();
+            }
+        }
+    }
+
+    protected virtual void Fire()
+    {
+        Debug.Log($"Fire {skillName.ToString()}!");
+        ProjectileManager.Instance.SpawnProjectile(skillName);
+    }
+
+    public static async UniTask DelayFloat(float delayInSeconds)
+    {
+        int delayInMilliseconds = (int)(delayInSeconds * 1000);
+
+        await UniTask.Delay(delayInMilliseconds);
+    }
+}
+
+
