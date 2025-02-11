a1b3f5e65aa7a64a1e0bff4094c04e522a44e316
DESKTOP-99CV4K8\qpalz <qpalzm60515@gmail.com>
2025-02-12 01:36:08 +0900

    [feat] 스크립트 추가 및 가호 로직 수정
    [body]
    - Bless스크립트 추가
    - 가호 UI 로직 수정중
    
    [todo]
    - 가호별 메서드 버튼에 리스너 등록

---
+++ Assets/_Scripts/Player/Bless.cs
@@ -0,0 +1,133 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bless : MonoBehaviour
+{
+    #region 공격가호
+
+    public void ATK_Increase()
+    {
+
+    }
+
+    public void Pojectile_Increase()
+    {
+
+    }
+
+    public void ATKSpeed_Increase()
+    {
+
+    }
+
+    public void CriticalDamage_Increase()
+    {
+
+    }
+
+    public void CriticalPercent_Increase()
+    {
+
+    }
+
+    public void Projectile_Destroy()
+    {
+
+    }
+
+    public void Projectile_Parry()
+    {
+
+    }
+
+    public void God_Kill()
+    {
+
+    }
+
+    #endregion
+
+    #region 방어가호
+
+    public void MaxHP_Increase()
+    {
+
+    }
+
+    public void Defense_Increase()
+    {
+
+    }
+
+    public void HPRegen_Increase()
+    {
+
+    }
+
+    public void Barrier_Activate()
+    {
+
+    }
+
+    public void Barrier_Cooldown()
+    {
+
+    }
+
+    public void Invincibility()
+    {
+
+    }
+
+    public void Adversary()
+    {
+
+    }
+
+    #endregion
+
+    #region 유틸가호
+
+    public void ATK_Range()
+    {
+
+    }
+
+    public void Duration()
+    {
+
+    }
+
+    public void Colldown()
+    {
+
+    }
+
+    public void Resurrection()
+    {
+
+    }
+
+    public void Magnet()
+    {
+
+    }
+
+    public void Growth()
+    {
+
+    }
+
+    public void Avarice()
+    {
+
+    }
+
+    public void Dash()
+    {
+
+    }
+
+    #endregion
+}
