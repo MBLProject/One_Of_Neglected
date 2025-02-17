a2defd98308454b02658caf68776406c6288e0bd
evespimrose <evespimrose@users.noreply.github.com>
2025-02-17 14:01:40 +0900

    [feat] PoisonShoes 구현
    [Body]
    - Aura랑 기본적으론 비슷하게 돌아갈 예정.
    - 다른점은 Aura는 맵에 하나만 존재, 얘는 복수로 존재 + 지속시간 있음

--- Assets/_Scripts/Player/Skill/Projectiles/PoisonShoesProjectiles.cs
+++ Assets/_Scripts/Player/Skill/Projectiles/PoisonShoesProjectiles.cs
@@ -1,8 +1,64 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Threading;
+using Cysharp.Threading.Tasks;
+using System.Linq;
 
-public class PoisonShoesProjectiles : Projectile
+public class PoisonShoesProjectile : Projectile
 {
+    private HashSet<MonsterBase> monstersInRange = new HashSet<MonsterBase>();
+    private float duration = 5f; // 잔류 시간
+    private float damagePerFrame = 0.5f; // 프레임 당 데미지
 
+    protected override void Start()
+    {
+        isMoving = true;
+        transform.position = startPosition; // 시작 위치 설정
+        cts = new CancellationTokenSource();
+        MoveProjectileAsync(cts.Token).Forget();
+        Destroy(gameObject, duration); // 일정 시간 후에 파괴
+    }
+
+    protected override async UniTaskVoid MoveProjectileAsync(CancellationToken token)
+    {
+        while (isMoving)
+        {
+            if (!GameManager.Instance.isPaused)
+            {
+                // 몬스터에게 데미지 주기
+                foreach (var monster in monstersInRange.ToList())
+                {
+                    monster.TakeDamage(damagePerFrame);
+                }
+                await UniTask.Yield(PlayerLoopTiming.Update);
+            }
+            else
+            {
+                await UniTask.Yield();
+            }
+        }
+    }
+
+    protected override void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.TryGetComponent<MonsterBase>(out var monster) && monstersInRange.Add(monster))
+        {
+            monster.OnDeath += RemoveMonsterFromSet;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.TryGetComponent<MonsterBase>(out var monster))
+        {
+            RemoveMonsterFromSet(monster);
+        }
+    }
+
+    private void RemoveMonsterFromSet(MonsterBase monster)
+    {
+        monstersInRange.Remove(monster);
+        monster.OnDeath -= RemoveMonsterFromSet;
+    }
 }
--- Assets/_Scripts/Player/Skill/Skills/PoisonShoes.cs
+++ Assets/_Scripts/Player/Skill/Skills/PoisonShoes.cs
@@ -1,8 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Threading;
+using Cysharp.Threading.Tasks;
+using System.Linq;
 
 public class PoisonShoes : Skill
 {
     public PoisonShoes(float defaultCooldown) : base(Enums.SkillName.PoisonShoes, defaultCooldown) { }
+
+    protected override async UniTask StartSkill()
+    {
+        isSkillActive = true;
+
+        while (isSkillActive)
+        {
+            if (!GameManager.Instance.isPaused)
+            {
+                // PoisonShoesProjectile을 바닥에 깔도록 요청
+                ProjectileManager.Instance.SpawnProjectile(skillName, damage, level, shotCount, projectileCount, projectileDelay, shotDelay);
+                await UniTask.Delay(TimeSpan.FromSeconds(defaultCooldown));
+            }
+            else
+            {
+                await UniTask.Yield();
+            }
+        }
+    }
 }
