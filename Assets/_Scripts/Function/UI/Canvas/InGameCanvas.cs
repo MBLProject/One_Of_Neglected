6d953382b22556b108c997045404ffe90fc625fb
DESKTOP-99CV4K8\qpalz <qpalzm60515@gmail.com>
2025-02-25 23:08:59 +0900

    [fix] 치트버튼
    [body]
    -게임씬 치트버튼(레벨업)

--- Assets/_Scripts/Function/UI/Canvas/InGameCanvas.cs
+++ Assets/_Scripts/Function/UI/Canvas/InGameCanvas.cs
@@ -1,8 +1,22 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class InGameCanvas : MonoBehaviour
 {
     public List<Panel> inGameCanvasPanels;
+    public Button cheat;
+
+    private void Awake()
+    {
+        cheat.onClick.AddListener(LevelUpBTN);
+    }
+
+    private void LevelUpBTN()
+    {
+        UnitManager.Instance.GetPlayer().statViewer.Level++;
+        inGameCanvasPanels[1].gameObject.SetActive(true);
+    }
 }
