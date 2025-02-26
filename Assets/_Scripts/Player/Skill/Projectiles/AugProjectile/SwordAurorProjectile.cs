f8548ad22b68719092c1c7fbe878f4723c8f8b05
csh-1234 <tmdtmdtmd4@naver.com>
2025-02-26 11:50:21 +0900

    [fix] 일부 증강 특수효과 수정

--- Assets/_Scripts/Player/Skill/Projectiles/AugProjectile/SwordAurorProjectile.cs
+++ Assets/_Scripts/Player/Skill/Projectiles/AugProjectile/SwordAurorProjectile.cs
@@ -4,13 +4,28 @@ using UnityEngine;
 
 public class SwordAurorProjectile : PlayerProjectile
 {
+    private bool isProjBreak = false;
     protected override void Start()
     {
         base.Start();
     }
 
+    public void SetProjBreak(bool projBreak)
+    {
+        isProjBreak = true;
+    }
+
     protected override void OnTriggerEnter2D(Collider2D collision)
     {
+       
+        if (collision.TryGetComponent<MonsterProjectile>(out var monsterProjectile))
+        {
+            if (isProjBreak)
+            {
+                monsterProjectile.Invoke("DestroyProjectile", 0f);
+            }
+        }
+
         if (collision.CompareTag("Monster"))
         {
             if (collision.TryGetComponent(out MonsterBase monster))
@@ -20,6 +35,7 @@ public class SwordAurorProjectile : PlayerProjectile
 
                 monster.TakeDamage(finalFinalDamage);
                 DataManager.Instance.AddDamageData(finalFinalDamage, Enums.AugmentName.TwoHandSword);
+                
 
                 if (stats.pierceCount > 0)
                 {
