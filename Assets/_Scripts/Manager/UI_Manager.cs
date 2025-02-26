3e41439f06d1c6525b9240a8ff96403a2b8047bf
KimChanYoung <qpalzm605@gmail.com>
2025-02-26 13:33:05 +0900

    [refactor] 화면비 조정 리펙
    [body]
    - 16 : 9 비율만 나오게 변경
    - 경험치 색상 랜덤 설정

--- Assets/_Scripts/Manager/UI_Manager.cs
+++ Assets/_Scripts/Manager/UI_Manager.cs
@@ -14,23 +14,31 @@ public class UI_Manager : Singleton<UI_Manager>
     public float effects_Value;
     [SerializeField] private TitleCanvas m_Title_Canvas;
     [SerializeField] private InGameCanvas m_InGame_Canvas;
+    [SerializeField] private LoadingCanvas m_Loading_Canvas;
+    [SerializeField] private CanvasScaler m_CanvasScaler;
     protected override void Awake()
     {
         base.Awake();
-
+        if (m_CanvasScaler == null) m_CanvasScaler = GetComponent<CanvasScaler>();
         SceneManager.sceneLoaded += (x, y) =>
         {
             if (x.name == "Title")
             {
                 panel_List.Clear();
                 panel_Dic.Clear();
+
                 m_Title_Canvas = FindAnyObjectByType<TitleCanvas>();
+                m_Title_Canvas.m_CanvasScaler.referenceResolution =
+                 new Vector2(Screen.currentResolution.width, Screen.currentResolution.height);
+
                 panel_List = m_Title_Canvas.titleCanvasPanels;
+
                 panel_List.Add(option_Panel);
                 foreach (Panel panel in panel_List)
                 {
                     panel_Dic.Add(panel.gameObject.name, panel);
                 }
+
             }
             if (x.name == "Game")
             {
@@ -38,12 +46,21 @@ public class UI_Manager : Singleton<UI_Manager>
                 panel_Dic.Clear();
 
                 m_InGame_Canvas = FindAnyObjectByType<InGameCanvas>();
+                m_InGame_Canvas.m_CanvasScaler.referenceResolution =
+                new Vector2(Screen.currentResolution.width, Screen.currentResolution.height);
+
                 panel_List = m_InGame_Canvas.inGameCanvasPanels;
                 panel_List.Add(option_Panel);
                 foreach (Panel panel in panel_List)
                 {
                     panel_Dic.Add(panel.gameObject.name, panel);
                 }
+            }
+            if (x.name == "LoadingScene")
+            {
+                m_Loading_Canvas = FindAnyObjectByType<LoadingCanvas>();
+                m_Loading_Canvas.m_CanvasScaler.referenceResolution =
+                new Vector2(Screen.currentResolution.width, Screen.currentResolution.height);
             }
         };
 
@@ -54,5 +71,18 @@ public class UI_Manager : Singleton<UI_Manager>
         panel_Dic["Option_Panel"].GetComponent<Option_Panel>().OnStart();
 
     }
-
+    public void SetCanvasScaler()
+    {
+        if (m_Title_Canvas != null)
+        {
+            m_Title_Canvas.m_CanvasScaler.referenceResolution =
+            new Vector2(Screen.currentResolution.width, Screen.currentResolution.height);
+        }
+        if (m_InGame_Canvas != null)
+        {
+            m_InGame_Canvas.m_CanvasScaler.referenceResolution =
+            new Vector2(Screen.currentResolution.width, Screen.currentResolution.height);
+        }
+        m_CanvasScaler.referenceResolution = new Vector2(Screen.currentResolution.width, Screen.currentResolution.height);
+    }
 }
