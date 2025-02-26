95bd5331dc4d61afe8de564c101970a1214a93da
evespimrose <evespimrose@users.noreply.github.com>
2025-02-26 16:31:24 +0900

    [fix] 투사체 갯수 반지 관련 오류 해결

--- Assets/_Scripts/Player/Skill/Skills/Shuriken.cs
+++ Assets/_Scripts/Player/Skill/Skills/Shuriken.cs
@@ -16,7 +16,7 @@ public class Shuriken : ActiveSkill
         playerStats.OnATKRangeChanged += (value) => stats.aTKRange = value;
         playerStats.OnCriRateChanged += (value) => stats.critical = value;
         playerStats.OnCriDamageChanged += (value) => stats.cATK = value;
-        playerStats.OnProjAmountChanged += (value) => stats.projectileCount += 1;
+        playerStats.OnProjAmountChanged += (value) => stats.projectileCount++;
         playerStats.OnDurationChanged += (value) => stats.lifetime *= value;
     }
 
